97d7024f72c0533b01a92952ea184bd178fae90f
Seoki <pkoi5088@gmail.com>
2023-09-04 01:41:35 +0900

    First Commit

---
+++ Server/Session/ClientSession.cs
@@ -0,0 +1,45 @@
+﻿using ServerCore;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class ClientSession : PacketSession
+    {
+        public int SessionId { get; set; }
+        public GameRoom Room { get; set; }
+        public float PosX { get; set; }
+        public float PosY { get; set; }
+        public float PosZ { get; set; }
+
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+
+            Program.Room.Push(() => Program.Room.Enter(this));
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            SessionManager.Instance.Remove(this);
+            if(Room != null)
+            {
+                GameRoom room = Program.Room;
+                room.Push(() => room.Leave(this));
+                Room = null;
+            }
+
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
---
+++ ServerCore/JobQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public interface IJobQueue
+    {
+        void Push(Action Job);
+    }
+
+    public class JobQueue : IJobQueue
+    {
+        Queue<Action> _jobQueue = new Queue<Action>();
+        object _lock = new object();
+        bool _flush = false;
+
+        public void Push(Action Job)
+        {
+            bool flush = false;
+
+            lock (_lock)
+            {
+                _jobQueue.Enqueue(Job);
+                if (_flush == false)
+                    flush = _flush = true;
+            }
+
+            if (flush)
+                Flush();
+        }
+
+        void Flush()
+        {
+            while (true)
+            {
+                Action action = Pop();
+                if (action == null)
+                {
+                    _flush = false;
+                    return;
+                }
+                action.Invoke();
+            }
+        }
+
+        Action Pop()
+        {
+            if (_jobQueue.Count == 0)
+            {
+                return null;
+            }
+            return _jobQueue.Dequeue();
+        }
+    }
+}
